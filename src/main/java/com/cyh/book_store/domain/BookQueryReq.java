9bac746cfe9a67823f58d713bce610f34adf4a09
YuHang Chen <alyyc78@email.nottingham.ac.uk>
2024-10-04 06:57:31 +0100

    书籍管理

--- src/main/java/com/cyh/book_store/domain/BookQueryReq.java
+++ src/main/java/com/cyh/book_store/domain/BookQueryReq.java
@@ -2,8 +2,6 @@ package com.cyh.book_store.domain;
 
 import lombok.Data;
 
-import java.math.BigDecimal;
-
 @Data
 public class BookQueryReq {
 
--- src/main/java/com/cyh/book_store/domain/BookResp.java
+++ src/main/java/com/cyh/book_store/domain/BookResp.java
@@ -1,9 +1,5 @@
 package com.cyh.book_store.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Data;
 
 import java.math.BigDecimal;
